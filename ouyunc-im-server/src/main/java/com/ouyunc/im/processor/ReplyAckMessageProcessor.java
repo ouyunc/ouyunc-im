fd28ef65b44a7697432b95aa48ad44b92436e8e1
方振勋 <664936598@qq.com>
2023-02-13 21:30:01 +0800

    v3.0.1 修复集群下客户收到消息后回复确认bug

--- ouyunc-im-server/src/main/java/com/ouyunc/im/processor/ReplyAckMessageProcessor.java
+++ ouyunc-im-server/src/main/java/com/ouyunc/im/processor/ReplyAckMessageProcessor.java
@@ -51,8 +51,11 @@ public class ReplyAckMessageProcessor extends AbstractMessageProcessor{
             }
             // 根据to从分布式缓存中取出targetServerAddress目标地址
             String to = message.getTo();
-            String comboIdentity = null;
-            String targetServerAddress = null;
+            // 这里需要前端传递两个值一个消息id一个消息目标的服务器登录设备类型（客户端可以从哪接收的消息获取）
+            ClientReplyAckContent clientReplyAckContent = JSONUtil.toBean(message.getContent(), ClientReplyAckContent.class);
+            byte deviceType = clientReplyAckContent.getDeviceType();
+            String comboIdentity = IdentityUtil.generalComboIdentity(to, deviceType);
+            String targetServerAddress;
             // 如果该消息是从其他服务传递过来的，则直接进行判断就行了，否则进行多端的转发或发送
             if (extraMessage.isDelivery()) {
                 // 如果是从其他服务传递过来的消息，则直接从目标服务取
@@ -62,10 +65,6 @@ public class ReplyAckMessageProcessor extends AbstractMessageProcessor{
                 if (MessageContentEnum.CLIENT_REPLY_ACK_CONTENT.type() != message.getContentType()) {
                     return;
                 }
-                // 这里需要前端传递两个值一个消息id一个消息目标的服务器登录设备类型（客户端可以从哪接收的消息获取）
-                ClientReplyAckContent clientReplyAckContent = JSONUtil.toBean(message.getContent(), ClientReplyAckContent.class);
-                byte deviceType = clientReplyAckContent.getDeviceType();
-                comboIdentity = IdentityUtil.generalComboIdentity(to, deviceType);
                 // 首先判断消息在线还是离线,只回复发送者设备
                 LoginUserInfo loginUserInfo = UserHelper.online(to, deviceType);
                 // 不在线,直接结束
@@ -82,7 +81,7 @@ public class ReplyAckMessageProcessor extends AbstractMessageProcessor{
             }
             MessageHelper.deliveryMessage(packet, SocketAddressUtil.convert2SocketAddress(targetServerAddress));
         });
-        // 这里如果返回ack给发送方，如果发送方不在线了，则不进行消息存储，问题不大。（可能造成的原因是：等发送方恢复在线了，可能会重复发送一条之前已经成功的消息，这个时候解后端需要做去重处理）
+        // 这里如果返回ack给发送方，如果发送方不在线了，则不进行消息存储，问题不大。（可能造成的原因是：等发送方恢复在线了，可能会重复发送一条之前已经成功的消息，这个时候客户端需要做去重处理）
     }
 
 
