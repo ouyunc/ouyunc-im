27c9f9b08aa9cd90ca66240b12e1986e158a6e8d
方振勋 <664936598@qq.com>
2023-06-28 15:09:42 +0800

    v4.0.0 1. 优化扫描包

--- ouyunc-im-server/src/main/java/com/ouyunc/im/StartIMServer.java
+++ ouyunc-im-server/src/main/java/com/ouyunc/im/StartIMServer.java
@@ -1,5 +1,8 @@
 package com.ouyunc.im;
 
+import com.ouyunc.im.processor.MessageProcessor;
+import com.ouyunc.im.utils.ClassScanner;
+
 /**
  * @Author fangzhenxun
  * @Description: 偶遇im的启动类总入口
@@ -8,6 +11,11 @@ public class StartIMServer {
 
     public static void main(String[] args) {
         IMServer imServer = new StandardIMServer();
+        try {
+            ClassScanner.scanPackageBySuper(MessageProcessor.class.getPackage().getName(), MessageProcessor.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         imServer.start();
     }
 
