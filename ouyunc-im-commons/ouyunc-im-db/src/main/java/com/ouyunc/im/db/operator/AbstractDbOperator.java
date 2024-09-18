fcac3af686e194c75de4a9692f76994b29e456ad
方振勋 <fang.zhenxun@gd-iot.com>
2024-09-18 09:20:57 +0800

    v6.0.0, jdk21 通讯框架

--- ouyunc-im-commons/ouyunc-im-db/src/main/java/com/ouyunc/im/db/operator/AbstractDbOperator.java
+++
@@ -1,8 +0,0 @@
-package com.ouyunc.im.db.operator;
-
-
-/**
- * 数据库操作抽象层
- */
-public abstract class AbstractDbOperator implements DbOperator{
-}
--- ouyunc-im-server/src/main/java/com/ouyunc/im/router/RouterStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.ouyunc.im.router;
-
-/**
- * @Author fangzhenxun
- * @Description: 路由策略接口
- **/
-public interface RouterStrategy {
-
-}
