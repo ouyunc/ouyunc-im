daccac7edd42f3d77ff004c41002b0f3523ca6fd
LAPTOP-M4BT07CJ\fzx <664936598@qq.com>
2024-10-04 20:59:21 +0800

    v6.0.0 添加客户端登录过期自定义时间

--- ouyunc-server/src/main/java/com/ouyunc/message/thread/ClientLoginKeepAliveThread.java
+++ ouyunc-server/src/main/java/com/ouyunc/message/thread/ClientLoginKeepAliveThread.java
@@ -8,18 +8,19 @@ import com.ouyunc.base.utils.IdentityUtil;
 import com.ouyunc.base.utils.TimeUtil;
 import com.ouyunc.cache.config.CacheFactory;
 import com.ouyunc.message.context.MessageServerContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * 客户端登录保活线程，消费者
  */
 public class ClientLoginKeepAliveThread implements Runnable{
 
-    private static final ExecutorService loginKeepAliveexEcutorService = Executors.newVirtualThreadPerTaskExecutor();
 
     private static final RedisTemplate<String, LoginContent> redisTemplate = CacheFactory.REDIS.instance();
 
@@ -29,22 +30,26 @@ public class ClientLoginKeepAliveThread implements Runnable{
     @SuppressWarnings("InfiniteLoopStatement")
     @Override
     public void run() {
-        long lastBatchSubmitTimeMillis = TimeUtil.currentTimeMillis();
+        AtomicLong lastBatchSubmitTimeMillis = new AtomicLong(TimeUtil.currentTimeMillis());
         int clientLoginInfoBatchExpireSize = MessageServerContext.serverProperties().getClientLoginInfoBatchExpireSize();
+        long clientLoginInfoScheduleTimeInterval = MessageServerContext.serverProperties().getClientLoginInfoScheduleTimeInterval();
         while (true) {
             // 这里用size 判断虽然会不准，但是没啥问题
-            if (MessageServerContext.clientKeepAliveQueue.size() >= clientLoginInfoBatchExpireSize || (lastBatchSubmitTimeMillis >= lastBatchSubmitTimeMillis + 1000)) {
-                // 多线程去执行操作redis
-                loginKeepAliveexEcutorService.submit(() -> {
-                    // 取出一千，有可能取不到，时间到了
-                    for (int i = 0; i < clientLoginInfoBatchExpireSize; i++) {
-                        LoginClientInfo loginClientInfo = MessageServerContext.clientKeepAliveQueue.poll();
-                        if (loginClientInfo != null) {
-                            redisTemplate.expire(CacheConstant.OUYUNC + CacheConstant.APP_KEY + loginClientInfo.getAppKey() + CacheConstant.COLON + CacheConstant.LOGIN + CacheConstant.USER + IdentityUtil.generalComboIdentity(loginClientInfo.getAppKey(), loginClientInfo.getIdentity(), loginClientInfo.getDeviceType()), loginClientInfo.getLoginExpireTime(), TimeUnit.SECONDS);
+            if (MessageServerContext.clientKeepAliveQueue.size() >= clientLoginInfoBatchExpireSize || (TimeUtil.currentTimeMillis() >= lastBatchSubmitTimeMillis.get() + clientLoginInfoScheduleTimeInterval)) {
+                redisTemplate.executePipelined(new SessionCallback<>() {
+                    @SuppressWarnings("unchecked")
+                    @Override
+                    public <K, V> Object execute(RedisOperations<K, V> operations) throws DataAccessException {
+                        for (int i = 0; i < clientLoginInfoBatchExpireSize; i++) {
+                            LoginClientInfo loginClientInfo = MessageServerContext.clientKeepAliveQueue.poll();
+                            if (loginClientInfo != null) {
+                                operations.expire((K) (CacheConstant.OUYUNC + CacheConstant.APP_KEY + loginClientInfo.getAppKey() + CacheConstant.COLON + CacheConstant.LOGIN + CacheConstant.USER + IdentityUtil.generalComboIdentity(loginClientInfo.getAppKey(), loginClientInfo.getIdentity(), loginClientInfo.getDeviceType())), loginClientInfo.getLoginExpireTime(), TimeUnit.SECONDS);
+                            }
                         }
+                        return null;
                     }
-
                 });
+                lastBatchSubmitTimeMillis.set(TimeUtil.currentTimeMillis());
             }
         }
     }
