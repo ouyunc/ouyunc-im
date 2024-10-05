6f5053694d3a8dcc85a0999838749ebdde37f1e8
LAPTOP-M4BT07CJ\fzx <664936598@qq.com>
2024-10-05 21:16:49 +0800

    v6.0.0 优化获取当前时间戳毫秒

--- ouyunc-commons/ouyunc-base/src/main/java/com/ouyunc/base/utils/TimeUtil.java
+++ ouyunc-commons/ouyunc-base/src/main/java/com/ouyunc/base/utils/TimeUtil.java
@@ -1,6 +1,6 @@
 package com.ouyunc.base.utils;
 
-import java.time.Clock;
+import org.apache.commons.lang3.time.StopWatch;
 
 /**
  * 时间工具类
@@ -13,6 +13,22 @@ public class TimeUtil {
      */
 
     public static long currentTimeMillis() {
-        return Clock.systemUTC().millis();
+        return SystemClock.now();
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        for (int t = 0; t < 100; t++) {
+            StopWatch stopWatch = StopWatch.createStarted();
+            // 获取一千万次时间
+            for (int i = 0; i < 10000000; i++) {
+                currentTimeMillis();
+            }
+            stopWatch.stop();
+            System.out.println(stopWatch.getTime());
+        }
     }
 }
