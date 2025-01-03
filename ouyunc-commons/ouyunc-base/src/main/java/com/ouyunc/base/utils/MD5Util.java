d9919e39a8cedf4a1ad54c425ac81b0fb1563c71
fangzhenxun <fangzhenxun@yto.net.cn>
2025-01-03 14:48:40 +0800

    v6.0.1  md5

--- ouyunc-commons/ouyunc-base/src/main/java/com/ouyunc/base/utils/MD5Util.java
+++ ouyunc-commons/ouyunc-base/src/main/java/com/ouyunc/base/utils/MD5Util.java
@@ -5,6 +5,7 @@ import org.apache.commons.codec.digest.DigestUtils;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * md5工具类
@@ -12,12 +13,35 @@ import java.io.IOException;
 public class MD5Util {
 
 
+
+    /**
+     * 计算字符串的MD5值
+     * @param text 待计算的字符串
+     * @return 计算结果，小写字母和数字
+     */
+    public static String md5(String text) {
+        return new String(DigestUtils.md5(text), StandardCharsets.UTF_8);
+    }
+
+    /**
+     * 计算字符串的MD5值
+     * @param file 待计算的字符串
+     * @return 计算结果,小写字母和数字
+     */
+    public static String md5(File file) {
+        try (FileInputStream fis = new FileInputStream(file)) {
+            return new String(DigestUtils.md5(fis), StandardCharsets.UTF_8);
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     /**
      * 计算字符串的MD5值
      * @param text 待计算的字符串
      * @return 计算结果，十六进制字符串格式
      */
-    public static String md5(String text) {
+    public static String md5Hex(String text) {
         return DigestUtils.md5Hex(text);
     }
 
@@ -27,9 +51,11 @@ public class MD5Util {
      * @return 计算结果，十六进制字符串格式
      * @throws IOException
      */
-    public static String md5(File file) throws IOException {
+    public static String md5Hex(File file) {
         try (FileInputStream fis = new FileInputStream(file)) {
             return DigestUtils.md5Hex(fis);
+        }catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
