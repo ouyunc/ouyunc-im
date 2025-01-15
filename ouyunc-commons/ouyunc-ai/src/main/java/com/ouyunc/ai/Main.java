046e3cdeba7627172c69b1de50835bdd2db8c971
fangzhenxun <fangzhenxun@yto.net.cn>
2025-01-15 15:41:04 +0800

    v6.1.0  更改输出到文件日志格式

--- ouyunc-commons/ouyunc-ai/src/main/java/com/ouyunc/ai/Main.java
+++ ouyunc-commons/ouyunc-ai/src/main/java/com/ouyunc/ai/Main.java
@@ -3,7 +3,7 @@ package com.ouyunc.ai;
 /**
  * @author fzx
  * @version 1.0
- * @description: 可以集成spring-ai 官方的sdk来接入和实现ai
+ * @description: 可以集成spring-ai 官方的sdk来接入和实现ai,或者使用alibaba 的依赖来接入
  * @date 2025/1/14 16:49
  */
 public class Main {
