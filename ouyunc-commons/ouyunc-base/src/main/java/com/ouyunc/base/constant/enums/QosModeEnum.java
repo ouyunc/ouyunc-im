0be4a5efe855b1be9245332e551fb8b34110fc91
fangzhenxun <fangzhenxun@yto.net.cn>
2025-01-24 08:46:28 +0800

    v6.1.0  js 客户端

--- ouyunc-commons/ouyunc-base/src/main/java/com/ouyunc/base/constant/enums/QosModeEnum.java
+++ ouyunc-commons/ouyunc-base/src/main/java/com/ouyunc/base/constant/enums/QosModeEnum.java
@@ -1,5 +1,7 @@
 package com.ouyunc.base.constant.enums;
 
+import com.ouyunc.base.constant.NumberConstant;
+
 /**
  * qos 模式
  */
@@ -7,11 +9,33 @@ public enum QosModeEnum {
     /**
      * 客户端模式
      */
-    CLIENT,
+    CLIENT(NumberConstant.NUMBER_1),
 
     /**
      * 服务端模式
      */
-    SERVER
+    SERVER(NumberConstant.NUMBER_2)
+    ;
+    private byte value;
 
+    QosModeEnum(byte value) {
+        this.value = value;
+    }
+
+    public byte getValue() {
+        return value;
+    }
+
+    public void setValue(byte value) {
+        this.value = value;
+    }
+
+    public static QosModeEnum prototype(byte value) {
+        for (QosModeEnum qosModeEnum : QosModeEnum.values()) {
+            if (qosModeEnum.getValue() == value) {
+                return qosModeEnum;
+            }
+        }
+        return null;
+    }
 }
