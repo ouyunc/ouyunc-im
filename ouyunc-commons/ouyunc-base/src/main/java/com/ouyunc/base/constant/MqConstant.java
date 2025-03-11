1b089ed9e64d701b3256b7fdc74fe01240696185
fangzhenxun <fangzhenxun@yto.net.cn>
2025-03-11 11:30:53 +0800

    v6.2.0  优化代码,mq

--- ouyunc-commons/ouyunc-base/src/main/java/com/ouyunc/base/constant/MqConstant.java
+++ ouyunc-commons/ouyunc-base/src/main/java/com/ouyunc/base/constant/MqConstant.java
@@ -10,6 +10,10 @@ public class MqConstant {
      * kafka 发送消息失败异常 topic
      */
     public static final String KAFKA_MESSAGE_SEND_FAIL_TOPIC = MessageConstant.OUYUNC + MessageConstant.UNDERLINE + "message_send_fail";
+    /**
+     * kafka 发送消息失败异常 group
+     */
+    public static final String KAFKA_MESSAGE_SEND_FAIL_GROUP = "message_send_fail";
 
     /**
      * kafka 异常 topic
@@ -17,9 +21,19 @@ public class MqConstant {
     public static final String KAFKA_EXCEPTION_TOPIC = MessageConstant.OUYUNC + MessageConstant.UNDERLINE + "exception";
 
     /**
+     * kafka 异常 group
+     */
+    public static final String KAFKA_EXCEPTION_GROUP = "exception";
+
+    /**
      * kafka 保存消息 topic
      */
     public static final String KAFKA_SAVE_MESSAGE_TOPIC = MessageConstant.OUYUNC + MessageConstant.UNDERLINE + "message_save";
+
+    /**
+     * kafka 保存消息 group
+     */
+    public static final String KAFKA_SAVE_MESSAGE_GROUP = "message_save";
 
     /**
      * kafka 撤销消息 topic
@@ -27,7 +41,17 @@ public class MqConstant {
     public static final String KAFKA_WITHDRAW_MESSAGE_TOPIC =  MessageConstant.OUYUNC + MessageConstant.UNDERLINE + "message_withdraw";
 
     /**
+     * kafka 撤销消息 group
+     */
+    public static final String KAFKA_WITHDRAW_MESSAGE_GROUP =  "message_withdraw";
+
+    /**
      * kafka 已读回执消息 topic
      */
     public static final String KAFKA_READ_RECEIPT_MESSAGE_TOPIC =  MessageConstant.OUYUNC + MessageConstant.UNDERLINE + "message_read_receipt";
+
+    /**
+     * kafka 已读回执消息 group
+     */
+    public static final String KAFKA_READ_RECEIPT_MESSAGE_GROUP =  "message_read_receipt";
 }
