7ae638601afb12b5cfd572d1a2a96f9debe3a72a
LAPTOP-M4BT07CJ\fzx <664936598@qq.com>
2025-09-14 15:51:04 +0800

    v6.5.0  添加群的限制1

--- ouyunc-server/src/main/java/com/ouyunc/message/processor/QosC2SMessageProcessor.java
+++ ouyunc-server/src/main/java/com/ouyunc/message/processor/QosC2SMessageProcessor.java
@@ -1,16 +1,21 @@
 package com.ouyunc.message.processor;
 
+import com.ouyunc.base.constant.enums.ExceptionCodeEnum;
 import com.ouyunc.base.constant.enums.MessageType;
 import com.ouyunc.base.constant.enums.MessageTypeEnum;
 import com.ouyunc.base.constant.enums.QosModeEnum;
 import com.ouyunc.base.packet.Packet;
 import com.ouyunc.base.packet.message.Message;
+import com.ouyunc.core.listener.event.ExceptionEvent;
 import com.ouyunc.core.listener.event.RemoveOfflineEvent;
 import com.ouyunc.message.context.MessageServerContext;
 import com.ouyunc.message.schedule.ScheduleTimer;
+import com.ouyunc.message.validator.AuthValidator;
+import com.ouyunc.message.validator.PermissionValidator;
 import io.netty.channel.ChannelHandlerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
 
 /**
  * @Author fzx
@@ -24,6 +29,39 @@ public final class QosC2SMessageProcessor extends AbstractMessageProcessor<Byte>
         return MessageTypeEnum.QOS_C2S_ACK;
     }
 
+    @Override
+    public void preProcess(ChannelHandlerContext ctx, Packet packet) {
+        // 异步存储packet（目前只是保存相关信息，不做扩展，以后可以做数据分析使用），这里将该数据存储到时序数据库中
+        repository().save(packet).whenComplete((sendResult, ex) -> {
+            if (ex == null) {
+                // 两个都校验通过才放行
+                if (!AuthValidator.INSTANCE.verify(packet, ctx)) {
+                    // 关闭当前 channel，这里会触发 DefaultSocketChannelInitializer 中的关闭逻辑
+                    log.error("校验消息失败: {} 认证未通过,开始关闭channel", packet);
+                    MessageServerContext.publishEvent(new ExceptionEvent(ExceptionCodeEnum.LOGIN_AUTH_ERROR, "登录认证未通过!", packet), true);
+                    ctx.close();
+                    return;
+                }
+                // 构建校验逻辑
+                PermissionValidator.INSTANCE.negate()
+                        .verify(packet, ctx)
+                        .onErrorResume(error -> {
+                            log.error("校验过程中出现异常: {}", error.getMessage());
+                            return Mono.just(true); // 出现异常时默认校验不通过
+                        }).flatMap(result -> {
+                            if (result) {
+                                log.warn("权限不足, 请知悉。该消息 {} 被忽略", packet);
+                                return Mono.empty(); // 校验不通过，不传递消息
+                            }
+                            return Mono.just(packet); // 校验通过，继续传递消息
+                        }).subscribe(ctx::fireChannelRead);
+            } else {
+                // 发送失败
+                log.error("Failed to send message: {} ", ex.getMessage());
+                MessageServerContext.publishEvent(new ExceptionEvent(ExceptionCodeEnum.MQ_PERSISTENCE_ERROR, "通过发送mq保存消息异常!", packet), true);
+            }
+        });
+    }
     /**
      * 外部客户端接收到消息后，发送消息已接收给服务端，做消息已接收确认
      */
@@ -33,6 +71,8 @@ public final class QosC2SMessageProcessor extends AbstractMessageProcessor<Byte>
             Message message = packet.getMessage();
             String receivedPackageId = message.getContent();
             log.info("QosC2SMessageProcessor 外部客户端接收到消息id: {}", receivedPackageId);
+            // 可以判断这个receivedPackageId是否合法，不是自己发送的，以及消息类型是合法的，这里不做过多的判断
+
             // 移除离线消息,通过异步发送移除离线消息事件
             MessageServerContext.publishEvent(new RemoveOfflineEvent(packet), true);
             // 停止本地的qos定时任务
