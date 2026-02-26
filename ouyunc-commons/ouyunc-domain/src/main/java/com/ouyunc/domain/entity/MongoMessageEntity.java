72cf022374e9a8670aa26c14bff5ce60711879c7
LAPTOP-M4BT07CJ\fzx <664936598@qq.com>
2026-02-26 22:09:49 +0800

    全局唯一id生成器

--- ouyunc-commons/ouyunc-domain/src/main/java/com/ouyunc/domain/entity/MongoMessageEntity.java
+++ ouyunc-commons/ouyunc-domain/src/main/java/com/ouyunc/domain/entity/MongoMessageEntity.java
@@ -27,6 +27,11 @@ public class MongoMessageEntity extends MessageEntity {
         this.expireAt = expireAt;
     }
 
+    public MongoMessageEntity(long id, byte protocol, byte protocolVersion, byte deviceType, byte networkType, byte encryptType, byte serializeAlgorithm, byte messageType, byte retain, String clientIp, String messageId, String from, int fromType, String to, int toType, int contentType, String content, int qos, String at, String ref, String extra, long clientSendTime, long serverArrivalTime, String appKey, LocalDateTime expireAt) {
+        super(id, protocol, protocolVersion, deviceType, networkType, encryptType, serializeAlgorithm, messageType, retain, clientIp, messageId, from, fromType, to, toType, contentType, content, qos, at, ref, extra, clientSendTime, serverArrivalTime, appKey);
+        this.expireAt = expireAt;
+    }
+
     public LocalDateTime getExpireAt() {
         return expireAt;
     }
