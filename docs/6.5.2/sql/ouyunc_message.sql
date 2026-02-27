c481e5f172cc3a77047988d55c00274a95f095d6
LAPTOP-M4BT07CJ\fzx <664936598@qq.com>
2026-02-27 20:42:36 +0800

    全局唯一id生成器

--- docs/6.5.2/sql/ouyunc_message.sql
+++ docs/6.5.2/sql/ouyunc_message.sql
@@ -20,7 +20,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 -- Table structure for ouyunc_im_app
 -- ----------------------------
-DROP TABLE IF EXISTS `ouyunc_im_app`;
+
 CREATE TABLE `ouyunc_im_app`  (
   `id` bigint(20) NOT NULL COMMENT '主键id',
   `app_key` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '客户端（外部平台）key  唯一',
@@ -45,7 +45,7 @@ INSERT INTO `ouyunc_im_app` VALUES (1, 'ouyunc', '123456', '偶云客', 1, 0, 1,
 -- ----------------------------
 -- Table structure for ouyunc_im_blacklist
 -- ----------------------------
-DROP TABLE IF EXISTS `ouyunc_im_blacklist`;
+
 CREATE TABLE `ouyunc_im_blacklist`  (
   `id` bigint(20) NOT NULL COMMENT '主键id',
   `identity` bigint(20) NOT NULL COMMENT '群或客户端唯一标识',
@@ -61,7 +61,7 @@ CREATE TABLE `ouyunc_im_blacklist`  (
 -- ----------------------------
 -- Table structure for ouyunc_im_friend
 -- ----------------------------
-DROP TABLE IF EXISTS `ouyunc_im_friend`;
+
 CREATE TABLE `ouyunc_im_friend`  (
   `id` bigint(20) NOT NULL COMMENT '主键id',
   `user_id` bigint(20) NULL DEFAULT NULL COMMENT '用户id',
@@ -79,7 +79,7 @@ CREATE TABLE `ouyunc_im_friend`  (
 -- ----------------------------
 -- Table structure for ouyunc_im_group
 -- ----------------------------
-DROP TABLE IF EXISTS `ouyunc_im_group`;
+
 CREATE TABLE `ouyunc_im_group`  (
   `id` bigint(20) NOT NULL COMMENT '主键id',
   `group_code` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '群号',
@@ -101,7 +101,7 @@ CREATE TABLE `ouyunc_im_group`  (
 -- ----------------------------
 -- Table structure for ouyunc_im_group_user
 -- ----------------------------
-DROP TABLE IF EXISTS `ouyunc_im_group_user`;
+
 CREATE TABLE `ouyunc_im_group_user`  (
   `id` bigint(20) NOT NULL COMMENT '主键id',
   `group_id` bigint(20) NOT NULL COMMENT '群组id',
@@ -125,48 +125,45 @@ CREATE TABLE `ouyunc_im_group_user`  (
 -- ----------------------------
 -- Table structure for ouyunc_im_message
 -- ----------------------------
-DROP TABLE IF EXISTS `ouyunc_im_message`;
-CREATE TABLE `ouyunc_im_message`  (
-  `id` bigint(20) NOT NULL COMMENT '主键消息id (packetId),服务端生成',
-  `protocol` tinyint(1) NOT NULL COMMENT '消息协议类型',
-  `protocol_version` tinyint(1) NOT NULL COMMENT '消息协议版本号',
-  `device_type` tinyint(1) NOT NULL COMMENT '设备类型',
-  `network_type` tinyint(1) NOT NULL COMMENT '网络类型',
-  `encrypt_type` tinyint(1) NOT NULL COMMENT '消息加密算法',
-  `serialize_algorithm` tinyint(1) NOT NULL COMMENT '消息内容序列化算法',
-  `message_type` tinyint(1) NOT NULL COMMENT '消息类型：心跳，群聊，私聊...',
-  `retain` tinyint(1) NOT NULL DEFAULT 0 COMMENT '保留位，1个字节',
-  `client_ip` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '客户端ip',
-  `message_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '客户端消息id',
-  `from` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '消息发送者',
-  `from_type` tinyint(1) NOT NULL COMMENT '发送方类型',
-  `to` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '消息接收者/群组',
-  `to_type` tinyint(1) NOT NULL COMMENT '接收方类型',
-  `content_type` int(11) NOT NULL COMMENT '消息内容类型: 文本，图片，音频...',
-  `content` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '消息内容',
-  `app_key` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '应用唯一标识',
-  `extra` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '消息内容扩展字段',
-  `at` json NULL COMMENT '@功能，这里存放客户端登录绑定的id',
-  `qos` tinyint(1) NOT NULL COMMENT '消息可靠性标识 qos = 0/1/2\r\n     * QoS 0：至多一次，at most once；发送方发送一条消息，接收方最多能接收到一次。即发送方完成消息发送之后不关心消息发送是否成功。\r\n     * QoS 1：至少一次，at least once；发送方发送一条消息，接收方至少能接收到一次。即发送方完成消息发送之后，若发送失败，则继续重发直到接受方接收到消息为止。这种模式下可能会导致接收方收到重复的消息。\r\n     * QoS 2：确保一次：exactly once；发送方发送一条消息，接收方一定且只能收到一次。即发送方完成消息发送之后，若发送失败，则继续重发直到接收方接收到消息为止，在这一过程中同时保证接收方不会因为消息重传而收到重复的消息。',
-  `client_send_time` bigint(20) NOT NULL COMMENT '消息发送时间戳',
-  `server_arrival_time` bigint(20) NOT NULL COMMENT '消息首次到达服务端时间戳',
-  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '默认删除，1-已删除，0-未删除',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `idx_from_to`(`from`, `to`) USING BTREE COMMENT 'from_to组合索引',
-  INDEX `idx_message_type`(`message_type`) USING BTREE COMMENT '消息类型索引',
-  INDEX `idx_client_send_time`(`client_send_time`) USING BTREE COMMENT '客户端发送时间索引',
-  INDEX `idx_server_arrival_time`(`server_arrival_time`) USING BTREE COMMENT '服务端到达时间索引',
-  INDEX `idx_to`(`to`) USING BTREE COMMENT 'to索引',
-  INDEX `idx_message_content_type`(`content_type`) USING BTREE COMMENT '消息内容类型索引',
-  INDEX `idx_app_key`(`app_key`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'im 消息业务全量存储表' ROW_FORMAT = Dynamic;
 
+CREATE TABLE `ouyunc_im_message` (
+                                     `id` bigint(20) NOT NULL COMMENT '主键消息id (packetId),服务端生成',
+                                     `protocol` tinyint(1) NOT NULL COMMENT '消息协议类型',
+                                     `protocol_version` tinyint(1) NOT NULL COMMENT '消息协议版本号',
+                                     `device_type` tinyint(1) NOT NULL COMMENT '设备类型',
+                                     `network_type` tinyint(1) NOT NULL COMMENT '网络类型',
+                                     `encrypt_type` tinyint(1) NOT NULL COMMENT '消息加密算法',
+                                     `serialize_algorithm` tinyint(1) NOT NULL COMMENT '消息内容序列化算法',
+                                     `message_type` tinyint(1) NOT NULL COMMENT '消息类型：心跳，群聊，私聊...',
+                                     `retain` tinyint(1) NOT NULL DEFAULT '0' COMMENT '保留位，1个字节',
+                                     `client_ip` varchar(40) NOT NULL COMMENT '客户端ip',
+                                     `message_id` varchar(64) NOT NULL COMMENT '客户端消息id',
+                                     `from` varchar(64) NOT NULL COMMENT '消息发送者',
+                                     `from_type` tinyint(1) NOT NULL COMMENT '发送方类型',
+                                     `to` varchar(64) DEFAULT NULL COMMENT '消息接收者/群组',
+                                     `to_type` tinyint(1) NOT NULL COMMENT '接收方类型',
+                                     `content_type` int(11) NOT NULL COMMENT '消息内容类型: 文本，图片，音频...',
+                                     `content` varchar(2000) DEFAULT NULL COMMENT '消息内容',
+                                     `app_key` varchar(64) NOT NULL COMMENT '应用唯一标识',
+                                     `extra` varchar(2000) DEFAULT NULL COMMENT '消息内容扩展字段',
+                                     `at` json DEFAULT NULL COMMENT '@功能，这里存放客户端登录绑定的id',
+                                     `qos` tinyint(1) NOT NULL COMMENT '消息可靠性标识 qos = 0/1/2\r\n     * QoS 0：至多一次，at most once；发送方发送一条消息，接收方最多能接收到一次。即发送方完成消息发送之后不关心消息发送是否成功。\r\n     * QoS 1：至少一次，at least once；发送方发送一条消息，接收方至少能接收到一次。即发送方完成消息发送之后，若发送失败，则继续重发直到接受方接收到消息为止。这种模式下可能会导致接收方收到重复的消息。\r\n     * QoS 2：确保一次：exactly once；发送方发送一条消息，接收方一定且只能收到一次。即发送方完成消息发送之后，若发送失败，则继续重发直到接收方接收到消息为止，在这一过程中同时保证接收方不会因为消息重传而收到重复的消息。',
+                                     `client_send_time` bigint(20) NOT NULL COMMENT '消息发送时间戳',
+                                     `server_arrival_time` bigint(20) NOT NULL COMMENT '消息首次到达服务端时间戳',
+                                     `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                     `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '默认删除，1-已删除，0-未删除',
+                                     PRIMARY KEY (`id`) USING BTREE,
+                                     KEY `idx_message_type` (`message_type`) USING BTREE COMMENT '消息类型索引',
+                                     KEY `idx_client_send_time` (`client_send_time`) USING BTREE COMMENT '客户端发送时间索引',
+                                     KEY `idx_server_arrival_time` (`server_arrival_time`) USING BTREE COMMENT '服务端到达时间索引',
+                                     KEY `idx_message_content_type` (`content_type`) USING BTREE COMMENT '消息内容类型索引',
+                                     KEY `idx_app_key_from_to` (`app_key`,`from`,`to`,`from_type`,`to_type`) USING BTREE COMMENT 'from_to组合索引'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='im 消息业务全量存储表';
 
 -- ----------------------------
 -- Table structure for ouyunc_im_message_withdraw
 -- ----------------------------
-DROP TABLE IF EXISTS `ouyunc_im_message_withdraw`;
+
 CREATE TABLE `ouyunc_im_message_withdraw`  (
   `id` bigint(20) NOT NULL COMMENT '主键',
   `withdrawn_time` bigint(20) NOT NULL COMMENT '撤回时间戳，单位毫秒',
@@ -178,7 +175,7 @@ CREATE TABLE `ouyunc_im_message_withdraw`  (
 -- ----------------------------
 -- Table structure for ouyunc_im_session_message_offset
 -- ----------------------------
-DROP TABLE IF EXISTS `ouyunc_im_session_message_offset`;
+
 CREATE TABLE `ouyunc_im_session_message_offset`  (
   `from` bigint(20) NOT NULL COMMENT '发送方ID',
   `to` bigint(20) NOT NULL COMMENT '接收方ID（用户或群组）',
@@ -196,7 +193,7 @@ INSERT INTO `ouyunc_im_session_message_offset` VALUES (1, 1, 1, 0, 0);
 -- ----------------------------
 -- Table structure for ouyunc_im_user
 -- ----------------------------
-DROP TABLE IF EXISTS `ouyunc_im_user`;
+
 CREATE TABLE `ouyunc_im_user`  (
   `id` bigint(20) NOT NULL COMMENT '主键id',
   `open_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '开放id',
