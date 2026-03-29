3f79950108f6330f830f64f31efad623148122db
LAPTOP-M4BT07CJ\fzx <664936598@qq.com>
2026-03-29 14:04:40 +0800

    类型

--- ouyunc-commons/ouyunc-base/src/main/java/com/ouyunc/base/constant/enums/EventRingEnum.java
+++ ouyunc-commons/ouyunc-base/src/main/java/com/ouyunc/base/constant/enums/EventRingEnum.java
@@ -9,13 +9,13 @@ import com.lmax.disruptor.dsl.ProducerType;
 public enum EventRingEnum {
 
     /** 高优先级、延迟敏感的事件（登录、支付等核心链路） */
-    FAST(2048, WaitStrategyMode.YIELDING, ProducerType.MULTI),
+    FAST(65536, WaitStrategyMode.YIELDING, ProducerType.MULTI),
 
     /** 普通业务事件（默认） */
-    NORMAL(1024, WaitStrategyMode.BLOCKING, ProducerType.MULTI),
+    NORMAL(1048576, WaitStrategyMode.BLOCKING, ProducerType.MULTI),
 
     /** 耗时较长的事件（日志、统计、持久化等） */
-    SLOW(512, WaitStrategyMode.BLOCKING, ProducerType.MULTI);
+    SLOW(262144, WaitStrategyMode.BLOCKING, ProducerType.MULTI);
 
     /**
      * RingBuffer 大小（必须是 2 的幂）
